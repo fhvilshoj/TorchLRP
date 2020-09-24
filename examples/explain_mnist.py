06126562390d85694cec75b1661cfd82097b280c
Frederik Hvilshøj <fhvilshoj@cs.au.dk>
2020-09-24 19:54:17 +0200

    Updated example also

--- examples/explain_mnist.py
+++ examples/explain_mnist.py
@@ -22,31 +22,66 @@ def store_patterns(file_name, patterns):
     with open(file_name, 'wb') as f:
         pickle.dump(patterns, f)
 
+
 def load_patterns(file_name): 
     with open(file_name, 'rb') as f: p = pickle.load(f)
     return p
 
-def prepare_batch_for_plotting(a, nrow=3, cmap='seismic'):
-    # Normalize
-    a /= torch.abs(a).view(a.size(0), -1).max(1)[0].view(-1, 1, 1, 1) + 1e-10
 
-    # Make image grid
-    grid = torchvision.utils.make_grid(a, nrow=nrow)
-    grid = grid.permute(1, 2, 0)
-    grid = grid.mean(-1)
+def project(X, output_range=(0, 1)):
+    absmax   = np.abs(X).max(axis=tuple(range(1, len(X.shape))), keepdims=True)
+    X       /= absmax + (absmax == 0).astype(float)
+    X        = (X+1) / 2. # range [0, 1]
+    X        = output_range[0] + X * (output_range[1] - output_range[0]) # range [x, y]
+    return X
 
-    return grid
+
+def heatmap(X, cmap_name="seismic"):
+    cmap = plt.cm.get_cmap(cmap_name)
+
+    if X.shape[1] in [1, 3]: X = X.permute(0, 2, 3, 1).detach().cpu().numpy()
+    if isinstance(X, torch.Tensor): X = X.detach().cpu().numpy()
+
+    shape = X.shape
+    tmp = X.sum(axis=-1)
+
+    tmp = project(tmp, output_range=(0, 255)).astype(int)
+    tmp = cmap(tmp.flatten())[:, :3].T
+    tmp = tmp.T
+
+    shape = list(shape)
+    shape[-1] = 3
+    return tmp.reshape(shape).astype(np.float32)
+
+
+def prepare_batch_for_plotting(a, nrow=3, fill_value=1., cmap_name="seismic"):
+    # Compute colors
+    a = heatmap(a, cmap_name=cmap_name) 
+    bs, h, w, c = a.shape
+
+    # Reshape to grid
+    rows = bs // nrow + int(bs % nrow != 0)
+    missing = (nrow - bs % nrow) % nrow
+    if missing > 0: # Fill empty spaces in the plot
+        a = np.concatenate([a, np.ones((missing, h, w, c))*fill_value], axis=0)
+
+    # Border around images
+    a = np.pad(a, ((0, 0), (1, 1), (1, 1), (0, 0)), 'constant', constant_values=0.5)
+    a = a.reshape(rows, nrow, h+2, w+2, c)
+    a = np.transpose(a, (0, 2, 1, 3, 4))
+    a = a.reshape( rows * (h+2), nrow * (w+2), c)
+    return a
+
 
 def plot_attribution(a, ax_, preds, title, cmap='seismic'):
-    b = 1.
-    if title != "Input": b = 10*((np.abs(a)**3.0).mean()**(1.0/3)) # L_3 norm? 
-    ax_.imshow(a, cmap=cmap, vmin=-b, vmax=b)
+    ax_.imshow(a) 
     ax_.axis('off')
+
     cols = (a.shape[1] - 2) // 30
     rows = (a.shape[0] - 2) // 30
     for i in range(rows):
         for j in range(cols):
-            ax_.text(28+j*30, 30+i*30, preds[i*cols+j].item(), horizontalalignment="right", verticalalignment="bottom", color="lime")
+            ax_.text(28+j*30, 28+i*30, preds[i*cols+j].item(), horizontalalignment="right", verticalalignment="bottom", color="lime")
     ax_.set_title(title)
 
 def main(args): 
@@ -57,8 +92,8 @@ def main(args):
     model = model.cpu()
     train_loader, test_loader = get_mnist_data(transform=torchvision.transforms.ToTensor(), batch_size=args.batch_size)
 
-    # Sample batch
-    for x, y in train_loader: break
+    # Sample batch from test_loader
+    for x, y in test_loader: break
     x = x[:num_samples_plot]
     y = y[:num_samples_plot]
     x.requires_grad_(True)
@@ -68,15 +103,20 @@ def main(args):
         pred = y_hat.max(1)[1]
 
     def run_and_plot_rule(rule, ax_, title=None, postprocess=None, pattern=None, cmap='seismic'): 
+
+        # # # # For the interested reader:
+        # This is where the LRP magic happens.
         # Reset gradient
         x.grad = None
 
-        # Forward pass and select argmax
+        # Forward pass with rule argument to "prepare" the explanation
         y_hat = model.forward(x, explain=True, rule=rule, pattern=pattern)
+        # Choose argmax
         y_hat = y_hat[torch.arange(x.shape[0]), y_hat.max(1)[1]]
+        # y_hat *= 0.5 * y_hat # to use value of y_hat as starting point
         y_hat = y_hat.sum()
 
-        # Backward pass
+        # Backward pass (compute explanation)
         y_hat.backward()
         attr = x.grad
 
@@ -84,14 +124,15 @@ def main(args):
             with torch.no_grad(): 
                 attr = postprocess(attr)
 
-        attr = prepare_batch_for_plotting(attr)
+        attr = prepare_batch_for_plotting(attr, cmap_name=cmap)
 
         if title is None: title = rule
         plot_attribution(attr, ax_, pred, title, cmap=cmap)
 
-    # Patterns
+
+    # # # # Patterns for PatternNet and PatternAttribution
     all_patterns_path = (base_path / 'examples' / 'pattern_all.pkl').as_posix()
-    if not os.path.exists(all_patterns_path): 
+    if not os.path.exists(all_patterns_path):  # Either load of compute them
         patterns_all = fit_patternnet(model, train_loader)
         store_patterns(all_patterns_path, patterns_all)
     else:
@@ -99,32 +140,32 @@ def main(args):
 
     pos_patterns_path = (base_path / 'examples' / 'pattern_pos.pkl').as_posix()
     if not os.path.exists(pos_patterns_path):
-        patterns_pos = fit_patternnet_positive(model, train_loader)
+        patterns_pos = fit_patternnet_positive(model, train_loader)#, max_iter=1)
         store_patterns(pos_patterns_path, patterns_pos)
     else:
         patterns_pos = load_patterns(pos_patterns_path)
 
-    # Plotting
+    # # # Plotting
     fig, ax = plt.subplots(2, 5, figsize=(10, 5))
 
     with torch.no_grad(): 
-        x_plot = prepare_batch_for_plotting(x*2.-1)
-        plot_attribution(x_plot, ax[0, 0], pred, "Input", cmap='gray')
+        x_plot = prepare_batch_for_plotting(x*2-1, cmap_name="gray")
+        plot_attribution(x_plot, ax[0, 0], pred, "Input")
 
-    # run_and_plot_rule("gradient", ax[0, 0])
+    # run_and_plot_rule("gradient", ax[1, 0], title="gradient")
     run_and_plot_rule("gradient", ax[1, 0], title="input $\\times$ gradient", postprocess = lambda attribution: attribution * x)
 
     run_and_plot_rule("epsilon", ax[0, 1])
     run_and_plot_rule("gamma+epsilon", ax[1, 1])
-
+# 
     run_and_plot_rule("alpha1beta0", ax[0, 2])
     run_and_plot_rule("alpha2beta1", ax[1, 2])
+# 
+    run_and_plot_rule("patternnet", ax[0, 3], pattern=patterns_all, title="PatternNet $S(x)$", cmap='gray')
+    run_and_plot_rule("patternnet", ax[1, 3], pattern=patterns_pos, title="PatternNet $S(x)_{+-}$", cmap='gray')
 
-    run_and_plot_rule("patternattribution", ax[0, 3], pattern=list(patterns_all), title="PatternAttribution $S(x)$")
-    run_and_plot_rule("patternattribution", ax[1, 3], pattern=list(patterns_pos), title="PatternAttribution $S(x)_{+-}$")
-
-    run_and_plot_rule("patternnet", ax[0, 4], pattern=patterns_all, title="PatternNet $S(x)$", cmap='gray')
-    run_and_plot_rule("patternnet", ax[1, 4], pattern=patterns_pos, title="PatternNet $S(x)_{+-}$", cmap='gray')
+    run_and_plot_rule("patternattribution", ax[0, 4], pattern=patterns_all, title="PatternAttribution $S(x)$")
+    run_and_plot_rule("patternattribution", ax[1, 4], pattern=patterns_pos, title="PatternAttribution $S(x)_{+-}$")
 
     fig.tight_layout()
 
@@ -134,7 +175,7 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("MNIST LRP Example")
-    parser.add_argument('--batch_size', type=int, default=500)
+    parser.add_argument('--batch_size', type=int, default=3000)
     parser.add_argument('--train_new', action='store_true', help='Train new predictive model')
     parser.add_argument('--epochs', '-e', type=int, default=5)
     parser.add_argument('--seed', '-d', type=int)
--- examples/utils.py
+++ examples/utils.py
@@ -18,16 +18,23 @@ _standard_transform = torchvision.transforms.Compose([
     # torchvision.transforms.Normalize((0.1307,), (0.3081,))
 ])
 
+class Identity(torch.nn.Module):
+    def forward(self, input):
+        return input
+
 def get_mnist_model():
     model = Sequential(
         Conv2d(1, 32, 3, 1, 1),
         nn.ReLU(),
+        # Identity(),
         Conv2d(32, 64, 3, 1, 1),
         nn.ReLU(),
+        # Identity(),
         MaxPool2d(2,2),
         nn.Flatten(),
         Linear(14*14*64, 512),
         nn.ReLU(),
+        # Identity(),
         Linear(512, 10)
     )
     return model
@@ -37,7 +44,7 @@ def get_mnist_data(transform, batch_size=32):
     train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=False)
 
     test = torchvision.datasets.MNIST((base_path / 'data').as_posix(), train=False, download=True, transform=transform)
-    test_loader = torch.utils.data.DataLoader(test, batch_size=batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test, batch_size=batch_size, shuffle=False)
     return train_loader, test_loader
 
 def prepare_mnist_model(model, model_path=(base_path / 'examples' / 'model.pth').as_posix(), epochs=1, lr=1e-3, train_new=False, transform=_standard_transform):
